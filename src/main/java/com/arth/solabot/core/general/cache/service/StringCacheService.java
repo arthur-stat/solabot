6b41393f233b9287a739dd80d5d0d491f02283ff
htrskmiku <oupanyu615@hotmail.com>
2026-03-15 17:38:43 +0800

    添加最小可运行早安晚安命令

---
+++ src/main/java/com/arth/solabot/core/general/cache/service/StringCacheService.java
@@ -0,0 +1,78 @@
+package com.arth.solabot.core.general.cache.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.DataType;
+import org.springframework.data.redis.core.*;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class StringCacheService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    /**
+     * 添加String键
+     * @param key
+     * @param value
+     * @param ttl_min
+     */
+    public void setStringKey(String key, String value, int ttl_min) {
+        redisTemplate.opsForValue().set(key, value, Duration.ofMinutes(ttl_min));
+    }
+
+    /**
+     * 获取String键
+     * @param key
+     * @return
+     */
+    public String getStringKey(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public List<String> getList(String key){
+        return redisTemplate.opsForList().range(key,0,-1);
+    }
+
+    public void listAppend(String key, String value) {
+        redisTemplate.opsForList().rightPush(key, value);
+    }
+
+    public boolean listHas(String key, String value) {
+        Long index = redisTemplate.opsForList().indexOf(key, value);
+        return index != null;
+    }
+
+    public Set<String> getMatchedKeySet(String pattern) {
+        return redisTemplate.execute((RedisCallback<? extends Set<String>>) connection -> {
+            Set<String> keySet = new HashSet<>();
+
+            ScanOptions options = KeyScanOptions
+                    .scanOptions(DataType.STRING)
+                    .match(pattern)
+                    .count(300L)
+                    .build();
+
+            try (Cursor<byte[]> cursor = connection.scan(options)) {
+                while (cursor.hasNext()) {
+                    String key = redisTemplate.getStringSerializer().deserialize(cursor.next());
+                    keySet.add(key);
+                }
+            }catch (Exception e){
+                log.error(e.getMessage(),e);
+            }
+
+            return keySet;
+        });
+    }
+}
---
+++ src/main/java/com/arth/solabot/plugin/custom/SleepingMeter.java
@@ -0,0 +1,153 @@
+package com.arth.solabot.plugin.custom;
+
+
+import com.arth.solabot.adapter.sender.Sender;
+import com.arth.solabot.adapter.sender.action.ActionChainBuilder;
+import com.arth.solabot.core.bot.dto.ParsedPayloadDTO;
+import com.arth.solabot.core.bot.invoker.annotation.BotCommand;
+import com.arth.solabot.core.bot.invoker.annotation.BotPlugin;
+import com.arth.solabot.core.general.cache.service.StringCacheService;
+import com.twelvemonkeys.lang.DateUtil;
+import jakarta.annotation.Nullable;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.buf.StringCache;
+
+import java.time.*;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Slf4j
+@BotPlugin(value = {"sleep","g"},glued = true)
+@RequiredArgsConstructor
+public class SleepingMeter extends Plugin {
+
+    private final StringCacheService stringCache;
+    private final Sender sender;
+    private final ActionChainBuilder actionChainBuilder;
+
+    private static final String rawKey = "sleeping:%s:%s";
+
+    private static final String morningTextSlept = " 早安！你睡了%d小时%d分钟%d秒，是群里第%s个早安的人！";
+    private static final String morningTextSleptNonRank = " 早上好！你睡了%d小时%d分钟%d秒";
+
+    private static final String nightText = " 晚安！";
+
+    private static final String morningTextNonSlept = " 早上好！";
+
+
+    @Override
+    public void index(ParsedPayloadDTO payload) {
+
+    }
+
+    @Override
+    public String getHelpText() {
+        return """
+               /gmorning ( /sleep morning ): 早安命令，若晚安过则同时输出睡觉时间
+               /gnight ( /sleep night ): 晚安命令
+               """;
+    }
+
+    @BotCommand({"早安","morning"})
+    public void morning(ParsedPayloadDTO payload) {
+        //sender.replyText(payload,"morning");
+        User sUser = getSleepingUser(payload.getGroupId(),payload.getUserId());
+        String replyText = "";
+        if (sUser.isSlept()) {
+            Duration duration = sUser.getSleepTime();
+            replyText = String.format(morningTextSleptNonRank ,duration.toHoursPart()
+                    ,duration.toMinutesPart()
+                    ,duration.toSecondsPart());
+        }else {
+            replyText = morningTextNonSlept;
+        }
+
+        ActionChainBuilder actionChain = actionChainBuilder.create()
+                .at(payload.getUserId())
+                .text(replyText);
+
+        String sendJson = payload.getMessageType().equals("group") ?
+                actionChain.toGroupJson(payload.getGroupId()) :
+                actionChain.toPrivateJson(payload.getUserId());
+
+        sender.pushActionJSON(payload.getSelfId(), sendJson);
+    }
+
+    @BotCommand({"晚安","night"})
+    public void night(ParsedPayloadDTO payload) {
+        setSleepingTime(payload.getGroupId(),payload.getUserId(),payload.getTime());
+
+        ActionChainBuilder actionChain = actionChainBuilder.create()
+                .at(payload.getUserId())
+                .text(nightText);
+
+        String sendJson = payload.getMessageType().equals("group")?
+                actionChain.toGroupJson(payload.getGroupId()) :
+                actionChain.toPrivateJson(payload.getUserId());
+
+        sender.pushActionJSON(payload.getSelfId(), sendJson);
+    }
+
+    private User getSleepingUser(long groupId,long userId){
+        long currentTime = LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8"));
+
+        AtomicLong atomicSleepingTimeLong = new AtomicLong(0L);
+        Optional<String> optionalString = Optional.ofNullable(getUserSleepingTime(groupId,userId));
+        optionalString.ifPresent(time-> atomicSleepingTimeLong.set(Long.parseLong(time)));
+
+        return new User(userId,atomicSleepingTimeLong.get(),currentTime,0);
+    }
+
+    /**
+     *  从redis获取用户晚安时间
+     * @param groupId
+     * @param userId
+     * @return String / null
+     */
+    @Nullable
+    private String getUserSleepingTime(long groupId,long userId){
+        return stringCache.getStringKey(getFormattedKey(groupId,userId));
+    }
+
+
+    /**
+     *  key : sleeping:(groupId):(userId) value: timestamp
+     * @param groupId
+     * @param userId
+     * @param timestamp
+     */
+
+    private void setSleepingTime(long groupId,long userId,long timestamp){
+        stringCache.setStringKey(getFormattedKey(groupId,userId),String.valueOf(timestamp),1440);
+    }
+
+    /**
+     * 格式化 key 为 sleeping:(groupId):(userId) 格式
+     * @param groupId
+     * @param userId
+     * @return
+     */
+    private static String getFormattedKey(long groupId,long userId){
+        return String.format(rawKey,groupId,userId);
+    }
+
+    private record User(long userId,long sleepTime,long getUpTime,int rank) {
+
+        public boolean isSlept(){
+            return sleepTime > 0 && sleepTime < getUpTime;
+        }
+        public Duration getSleepTime() {
+            if (!isSlept()) {
+                return Duration.ZERO;
+            }
+            //默认Asia,Shanghai +8
+            LocalDateTime sleep = LocalDateTime.ofInstant(Instant.ofEpochSecond(sleepTime), ZoneId.of("+8"));
+            LocalDateTime now = LocalDateTime.ofInstant(Instant.ofEpochSecond(getUpTime), ZoneId.of("+8"));
+            return Duration.between(sleep, now);
+        }
+
+    }
+
+}
